c315383037abf8de1ccd4848fec0e0c2e8af7b02
wahajali <wahajali@gmail.com>
2015-03-11 13:32:09 +0500

    Removed stock_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :stock_items
-
   resources :bs_or_pl_sub_categories
 
   get 'business_categories/get_categories' => 'business_categories#get_categories'
